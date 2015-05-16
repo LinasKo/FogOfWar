281b6d7ccc1e85968f69fc8caed9f388b965ea19
LinasKo <linaskondrackis@gmail.com>
2015-05-17 00:32:18 +0100

    not sure what this is :DDD

--- core/src/com/fog/screens/GameScreen.java
+++ core/src/com/fog/screens/GameScreen.java
@@ -3,7 +3,10 @@ package com.fog.screens;
 import java.util.HashMap;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -16,7 +19,7 @@ import com.fog.structures.Building;
 import com.fog.structures.Resource;
 import com.fog.structures.Structure;
 
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, InputProcessor {
 
 	private static float UNIT_SCALE = 1 / 32f; // used by camera
 	private static int PLAYER;
@@ -97,7 +100,7 @@ public class GameScreen implements Screen {
 		 * with the number of layers.
 		 */
 		
-		
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -137,5 +140,55 @@ public class GameScreen implements Screen {
 	public void dispose() {
 		renderer.dispose();
 		map.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if (button == Buttons.LEFT) {
+			// TODO: implement fog of war cleansing
+		}
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
