fab66b4ff82bacfbd1bc43cf7b744c00e4f5b86f
LinasKo <linaskondrackis@gmail.com>
2015-06-04 00:43:34 +0100

    Imported Tile map. Created simple prefabs for map, camera, GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public MeshRenderer redFogRenderer;
+	public MeshRenderer blueFogRenderer;
+
+	// Use this for initialization
+	void Start () {
+		redFogRenderer.enabled = false;
+		blueFogRenderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
