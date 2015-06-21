2b84c49fc4e924e277d8a5df71ada7f7c26a2dc2
Vytautas Mizgiris <vmizgiris@hotmail.com>
2015-06-21 21:10:34 +0100

    Player init class

---
+++ Assets/Scripts/PlayerInit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInit : MonoBehaviour {
+
+    public int playerWood, playerExp, playerHealth;
+
+	// Use this for initialization
+	void Start () {
+        playerWood = 100;
+        playerExp = 300;
+        playerHealth = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
