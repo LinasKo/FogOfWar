ebf1cc2c2be62b75f1a5e08eba6da6f63a5e0fa2
Vytautas Mizgiris <vmizgiris@hotmail.com>
2015-06-25 23:53:42 +0100

    Created a complete UI for Main Menu (with buttons)

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MovieTexture movie = (MovieTexture)GetComponent<MeshRenderer>().material.mainTexture;
+        movie.loop = true;
+        movie.Play();
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
