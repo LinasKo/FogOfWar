69b5c1b05c3fe122a3a17a58b261edadc83d7395
Vytautas Mizgiris <vmizgiris@hotmail.com>
2015-07-03 19:49:43 +0100

    Started implementing units

---
+++ Assets/Scripts/Gatherer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gatherer : MonoBehaviour
+{
+
+    // Added some things just to test rendering (not rendering) in fog of war. These can be deleted if class extends Unit.
+    private Renderer rend;
+    private FogOfWar fog;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        fog = FogOfWar.FindExisting;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Lock z axis so it wouldn't roll? right? TODO - find out what I did here.
+        transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z);
+        if (fog.IsFoggy(transform.position))
+        {
+            rend.enabled = false;
+        }
+        else
+        {
+            rend.enabled = true;
+        }
+    }
+}
